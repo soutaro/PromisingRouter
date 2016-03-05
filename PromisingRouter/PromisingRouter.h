5ed95530b36ff511ba48aedb16d37c4cff0a76ab
Soutaro Matsumoto <matsumoto@soutaro.com>
2016-03-05 22:21:05 +0900

    Initial Commit

---
+++ PromisingRouter/PromisingRouter.h
@@ -0,0 +1,19 @@
+//
+//  PromisingRouter.h
+//  PromisingRouter
+//
+//  Created by 松本宗太郎 on 2016/03/05.
+//  Copyright © 2016年 Soutaro Matsumoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PromisingRouter.
+FOUNDATION_EXPORT double PromisingRouterVersionNumber;
+
+//! Project version string for PromisingRouter.
+FOUNDATION_EXPORT const unsigned char PromisingRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PromisingRouter/PublicHeader.h>
+
+
